7b9fada9afddc7805c60df7a37ed99be9e98cdc6
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-07-31 23:53:03 +0530

    Created Variable class and add Junit dependency

--- src/main/java/introduction1/HelloWorld1.java
+++ src/main/java/introduction1/HelloWorld1.java
@@ -1,13 +1,17 @@
 package introduction1;
 
-
 /**
  * @author Sahil Afrid Farookhi
- * A simple "Hello World!" Java Program.
+ * 
+ *         A simple "Hello World!" Java Program.
  */
-public class HelloWorld {
+public class HelloWorld1 {
 
 	public static void main(String[] args) {
+		helloWorld();
+	}
+
+	public static void helloWorld() {
 		System.out.println("Hello World!");
 	}
 
