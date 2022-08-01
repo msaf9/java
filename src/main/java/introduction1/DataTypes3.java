a3a78a0398eb64c732f6dd4d57885cff9c76e91d
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-08-01 23:41:43 +0530

    Added DataTypes Java class

---
+++ src/main/java/introduction1/DataTypes3.java
@@ -0,0 +1,19 @@
+package introduction1;
+
+public class DataTypes3 {
+
+	public static void main(String[] args) {
+		int numberFive = 5;
+		float numberWithDecimalPoints = 5.99f;
+		char letterD = 'D';
+		boolean trueBooleanValue = true;
+		String stringValue = "Hello";
+
+		System.out.println("numberFive: " + numberFive);
+		System.out.println("numberWithDecimalPoints: " + numberWithDecimalPoints);
+		System.out.println("letterD: " + letterD);
+		System.out.println("trueBooleanValue: " + trueBooleanValue);
+		System.out.println("stringValue: " + stringValue);
+	}
+
+}
