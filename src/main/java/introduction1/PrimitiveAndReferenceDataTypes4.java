f4ac77f19a3933d526857af7c56ccd2ff9aa2a94
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-08-03 23:26:40 +0530

    WIP: Added interface reference class

--- src/main/java/introduction1/PrimitiveAndReferenceDataTypes4.java
+++ src/main/java/introduction1/PrimitiveAndReferenceDataTypes4.java
@@ -2,17 +2,18 @@ package introduction1;
 
 import java.util.Arrays;
 
-public class PrimitiveAndReferenceDataTypes4 {
+public class PrimitiveAndReferenceDataTypes4 implements ReferenceTypeInterface4_2 {
 
 	public static void main(String[] args) {
 		
 		ReferenceTypeClass4_1 referenceTypeClass = new ReferenceTypeClass4_1();
-
+		
 		int intArray[] = { 1, 2, 3 };
 		int classReferenceDataType = referenceTypeClass.classVariable;
 
 		System.out.println("Array:" + Arrays.toString(intArray));
 		System.out.println("Class Reference Data Type:" + classReferenceDataType);
+		System.out.println("Interface:" + booleanValueTrue);
 	}
 
 }
---
+++ src/main/java/introduction1/ReferenceTypeInterface4_2.java
@@ -0,0 +1,5 @@
+package introduction1;
+
+public interface ReferenceTypeInterface4_2 {
+	boolean booleanValueTrue = true;
+}
