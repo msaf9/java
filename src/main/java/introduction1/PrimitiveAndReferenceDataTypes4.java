8768a6f06dceb3ca34cfe60808d2d770ed585efe
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-08-04 23:50:46 +0530

    WIP: Added String

--- src/main/java/introduction1/PrimitiveAndReferenceDataTypes4.java
+++ src/main/java/introduction1/PrimitiveAndReferenceDataTypes4.java
@@ -10,10 +10,12 @@ public class PrimitiveAndReferenceDataTypes4 implements ReferenceTypeInterface4_
 		
 		int intArray[] = { 1, 2, 3 };
 		int classReferenceDataType = referenceTypeClass.classVariable;
+		String stringForExample = "This is an example string variable value";
 
 		System.out.println("Array:" + Arrays.toString(intArray));
 		System.out.println("Class Reference Data Type:" + classReferenceDataType);
 		System.out.println("Interface:" + booleanValueTrue);
+		System.out.println("stringForExample:" + stringForExample);
 	}
 
 }
