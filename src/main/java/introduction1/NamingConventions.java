46a5303a87c73cf028ffa17ab6940ad631d2e4c8
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-08-10 23:26:44 +0530

    Added Naming conventions class

---
+++ src/main/java/introduction1/NamingConventions.java
@@ -0,0 +1,24 @@
+package introduction1;
+
+public class NamingConventions {
+
+	public static void main(String[] args) {
+		NamingConventions namingConventions = new NamingConventions();
+
+		System.out.println(namingConventions.camelCasingMethodName());
+
+		System.out.println(PascalCasingInterfaceName.camelCasingVariable);
+		System.out.print("Macro casing is followed for CONSTANT values in Java: ");
+		System.out.println(PascalCasingInterfaceName.CONSTANT_VALUE);
+	}
+
+	public String camelCasingMethodName() {
+		return "Camel Casing is followed for method names in Java.";
+	}
+
+	interface PascalCasingInterfaceName {
+		public String camelCasingVariable = "Camel Casing is followed for variable names and Pascal casing for interface names in Java.";
+		public final int CONSTANT_VALUE = 11;
+	}
+
+}
