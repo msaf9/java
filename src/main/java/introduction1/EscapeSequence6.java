07b4db2022d8197effd0bd843a2b7f216c667f32
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-08-11 17:49:06 +0530

    Added Escape sequence class

---
+++ src/main/java/introduction1/EscapeSequence6.java
@@ -0,0 +1,17 @@
+package introduction1;
+
+public class EscapeSequence6 {
+
+	public static void main(String[] args) {
+		System.out.println("In Java, to perform escapse sequence \\ '\' is used.");
+		System.out.println("To insert tab space, use \\t '\t'.");
+		System.out.println("To inset backspace, use \\b \b.");
+		System.out.println("To insert newline, use \\n \n 'New line.'");
+		System.out.println("For carriage return character, use \\r '\r Next line.'");
+		System.out.println("For page break, use \\f \f This is form break.");
+		System.out.println("For double quotes, use \\\" \"This is double quote statement\". ");
+		System.out.println("For single quotes, use \\\' \'This is single quote statement\'. ");
+		System.out.println("To insert backslash, use \\ '\\'.");
+	}
+
+}
