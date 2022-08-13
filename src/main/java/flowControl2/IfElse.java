b0b3d56c44ce69d7a5154655170dd77f42dcd99f
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-08-13 12:51:26 +0530

    Added IfElse class

--- src/main/java/flowControl2/IfElse.java
+++ src/main/java/flowControl2/IfElse.java
@@ -1,14 +1,62 @@
 package flowControl2;
 
+import java.util.Scanner;
+
 /**
- * @author Sahil Afrid Farookhi
- * A Java program to understand the If and Else Flow control
+ * @author Sahil Afrid Farookhi A Java program to understand the If and Else
+ *         Flow control
  */
 public class IfElse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		IfElse ifElse = new IfElse();
+		int inputNumber1, inputNumber2;
+		System.out.print("Enter your first number:");
+		inputNumber1 = sc.nextInt();
+		System.out.print("Enter your second number:");
+		inputNumber2 = sc.nextInt();
+		if (!(inputNumber1 == inputNumber2)) {
+			if (inputNumber1 > 0 && inputNumber2 > 0) {
+				if (ifElse.compareTwoIntegers(inputNumber1, inputNumber2)) {
+					System.out.println(inputNumber1 + " is bigger than " + inputNumber2 + ".");
+				} else {
+					System.out.println(inputNumber2 + " is bigger than " + inputNumber1 + ".");
+				}
+			} else if (inputNumber1 < 0 || inputNumber2 < 0) {
+				if (inputNumber1 < 0 && inputNumber2 < 0) {
+					System.out.println(
+							"Both the numbers " + inputNumber1 + " and " + inputNumber2 + " are less than zero.");
+				} else if (inputNumber1 < 0) {
+					System.out.println(inputNumber1 + " is less than zero.");
+				} else {
+					System.out.println(inputNumber2 + " is less than zero.");
+				}
+			} else if (inputNumber1 == 0 || inputNumber2 == 0) {
+				if (inputNumber1 == 0 && inputNumber2 == 0) {
+					System.out.println(
+							"Both the numbers " + inputNumber1 + " and " + inputNumber2 + " are less than zero.");
+				} else if (inputNumber1 == 0) {
+					System.out.println(inputNumber1 + " is less than zero.");
+				} else {
+					System.out.println(inputNumber2 + " is less than zero.");
+				}
+			}
+		} else {
+			System.out.println("Both the numbers " + inputNumber1 + " and " + inputNumber2 + " are equal.");
+		}
+		sc.close();
 
+	}
+
+	public boolean compareTwoIntegers(int number1, int number2) {
+		boolean resultFlag = false;
+		if (number1 > number2) {
+			resultFlag = true;
+		} else {
+			resultFlag = false;
+		}
+		return resultFlag;
 	}
 
 }
