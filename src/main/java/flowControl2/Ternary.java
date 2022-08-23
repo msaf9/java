4ab4a3becb8592627a54d1219c612296d317bc37
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-08-23 14:56:02 +0530

    Added Ternary class

---
+++ src/main/java/flowControl2/Ternary.java
@@ -0,0 +1,20 @@
+package flowControl2;
+
+import java.util.Scanner;
+
+public class Ternary {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int inputNumber;
+		String result = "";
+
+		System.out.println("Know a number is positive or negative");
+		System.out.print("Enter your number (Zero is either positive nor negative):");
+		inputNumber = sc.nextInt();
+		result = inputNumber > 0 ? "Positive Number" : "Negative Number";
+		System.out.println(result);
+
+		sc.close();
+	}
+}
