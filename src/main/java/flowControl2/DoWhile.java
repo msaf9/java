deb8574d588e0970de1cf16e7a3c47e4686351da
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-08-28 18:16:21 +0530

    Added DoWhile class

---
+++ src/main/java/flowControl2/DoWhile.java
@@ -0,0 +1,24 @@
+package flowControl2;
+
+import java.util.Scanner;
+
+public class DoWhile {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int number, result = 1;
+
+		System.out.print("Enter number to find factorial:");
+		number = sc.nextInt();
+		if (number == 1 || number == 0) {
+			result = 1;
+		} else {
+			do {
+				result *= number;
+				number--;
+			} while (number != 0);
+		}
+
+		System.out.println("Factorial:" + result);
+		sc.close();
+	}
+}
