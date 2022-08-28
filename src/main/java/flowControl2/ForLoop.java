09e59fb3e0bcf033b6d18915a043e03b8b4754ed
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-08-28 18:23:04 +0530

    Added ForLoop class

---
+++ src/main/java/flowControl2/ForLoop.java
@@ -0,0 +1,15 @@
+package flowControl2;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		String[] stringArray = { "Java", "Ruby", "C", "JavaScript" };
+
+		System.out.println("Printing stringArray elements in reverse order:");
+		for (int i = stringArray.length - 1; i >= 0; i--) {
+			System.out.println(i+1 + ". " + stringArray[i]);
+		}
+
+	}
+
+}
