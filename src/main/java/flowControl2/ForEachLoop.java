7ad9c5d3cf3906890a0373b383bd8f5d2aa5edaa
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-08-29 18:24:27 +0530

    Added ForEach class

---
+++ src/main/java/flowControl2/ForEachLoop.java
@@ -0,0 +1,14 @@
+package flowControl2;
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		String[] fruitsArray = {"Orange", "Mango", "Banana", "Apple", "Grapes" };
+		
+		System.out.println("Fruits from String Array:");
+		for(String x : fruitsArray) {			
+			System.out.println(x);
+		}
+	}
+
+}
