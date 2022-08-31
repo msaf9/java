639b94fb5319c007b4aa62d0f8f98f71fd26f61d
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-08-31 23:18:24 +0530

    Added NestedLoop class

---
+++ src/main/java/flowControl2/NestedLoops.java
@@ -0,0 +1,17 @@
+package flowControl2;
+
+public class NestedLoops {
+
+	public static void main(String[] args) {
+		int week = 2, day = 3;
+
+		for (int i = 1; i <= week; i++) {
+			System.out.println("Week " + i);
+			for (int j = 1; j <= day; j++) {
+				System.out.println("  Day " + j);
+			}
+			System.out.println();
+		}
+	}
+
+}
