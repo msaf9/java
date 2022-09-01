c73e22c34755343e781c91df6fa836949239d24e
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-09-01 13:08:31 +0530

    Added SwitchBreak class

---
+++ src/main/java/flowControl2/SwitchBreak.java
@@ -0,0 +1,26 @@
+package flowControl2;
+
+import java.util.Scanner;
+
+public class SwitchBreak {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int option;
+		System.out.print("Enter your option:");
+		option = sc.nextInt();
+
+		switch (option) {
+		case 1:
+			System.out.println("You have selected option " + option);
+			break;
+		case 2:
+			System.out.println("You have selected option " + option);
+			break;
+		default:
+			System.out.println("Select only 1 or 2.");
+		}
+		sc.close();
+	}
+
+}
