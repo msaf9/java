8854c880e19b4b4bad7382187f21c82fd5c472bc
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-09-03 18:42:33 +0530

    Added Break class and modified SwitchBreak class to Switch class

--- src/main/java/flowControl2/Switch.java
+++ src/main/java/flowControl2/Switch.java
@@ -2,7 +2,7 @@ package flowControl2;
 
 import java.util.Scanner;
 
-public class SwitchBreak {
+public class Switch {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
