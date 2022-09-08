5f0a0ba8a5c39d0b2ee3b7972f9477b32f13e24f
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-09-08 14:55:27 +0530

    Added SyntaxOfMethod class

--- src/main/java/methods3/SyntaxOfMethod.java
+++ src/main/java/methods3/SyntaxOfMethod.java
@@ -1,10 +1,28 @@
 package methods3;
 
+import java.util.Scanner;
+
 public class SyntaxOfMethod {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int number;
+		System.out.print("Enter you number to check wheather it is Even or Odd:");
+		number = sc.nextInt();
+		if (checkOddEven(number)) {
+			System.out.println(number + " is an Even number.");
+		} else {
+			System.out.println(number + " is an Odd number.");
+		}
+		sc.close();
+	}
 
+	public static boolean checkOddEven(int number) {
+		boolean evenBool = false;
+		if (number % 2 == 0) {
+			evenBool = true;
+		}
+		return evenBool;
 	}
 
 }
